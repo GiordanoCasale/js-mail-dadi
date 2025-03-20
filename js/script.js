cb9f321e9beb1687bce61c6651568d9cd641c2b5
GiordanoCasale <giordanocasale8@gmail.com>
2025-03-20 14:29:30 +0100

    [UPD] Array created

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//primo esercizio
+//creiamo un array con la lista delle email degli invitati
+const list = ["peppescoppiato@libero.it", "ginobuonvino@hotmail.com", "monkeydluffy@gmail.com", "kenkaneki@vattelaapesca.com", "paolobianchi@bellafrate.it", "mariobros@fungolandia.it", "axelblaze@soccer.com"]
+//andiamo a definire la variabile con il prompt
+let email = prompt("inserisci la mail dove è stato inviato l'invito")
