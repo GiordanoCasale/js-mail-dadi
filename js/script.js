39f9c3896f6ef3fd3306d335db6498d63afd8c5e
GiordanoCasale <giordanocasale8@gmail.com>
2025-03-20 15:15:13 +0100

    [UPD] First exercise completed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
 //primo esercizio
 //creiamo un array con la lista delle email degli invitati
-const list = ["peppescoppiato@libero.it", "ginobuonvino@hotmail.com", "monkeydluffy@gmail.com", "kenkaneki@vattelaapesca.com", "paolobianchi@bellafrate.it", "mariobros@fungolandia.it", "axelblaze@soccer.com"]
+const list = ["peppescoppiato@libero.it", "ginobuonvino@hotmail.com", "monkeydluffy@gmail.com", "kenkaneki@vattelaapesca.com", "paolobianchi@bellafrate.it", "mariobros@fungolandia.it", "axelblaze@soccer.com"];
 //andiamo a definire la variabile con il prompt
-let email = prompt("inserisci la mail dove è stato inviato l'invito")
+let email = prompt("inserisci la mail dove è stato inviato l'invito");
+//andiamo a definire la variabile che ci permetterà di fare il check
+let checkmail = false;
+//facciamo ciclare la lista con il for
+for(let i=0; i<list.length; i++){
+    if(list[i] == email){
+        checkmail = true;    
+    }
+}
+//andiamo a confermare il controllo in base alla variabile
+if(checkmail){
+    console.log("Presenza nella lista confermata")
+}
+else{
+    console.log("Nessun riscontro all'interno della lista")
+}
+//fine primo esercizio
+
+//secondo esercizio
+//andiamo a creare le variabili per l'utente e il pc
+const user= Math.floor((Math.random() * 6) + 1);
+const pc= Math.floor((Math.random() * 6) + 1);
+//andiamo a mettere le condizioni del risultato con if
+if(user>pc){
+    console.log("Vince User")
+}
+
+else(user<pc){
+    console.log("Vince Pc")
+}
+
+else if(user==pc){
+    console.log("Pareggio")
+}
+
